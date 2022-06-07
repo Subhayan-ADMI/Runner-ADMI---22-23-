2ee88a7f31b7c28600b75ad00f8ca4329776322a
Auravision <auravision.africa@gmail.com>
2022-06-07 15:57:22 +0300

    Commit of Waypoints system using Vector2.MoveTowards() to implement an Obstacle movement system.
    Students went through a review of Methods,Method Parameters and Arguments, Method return calls, Unity Monobehaviour callback methods and Order of execution, and were introduced to the Array and how to iterate through an array using a counter variable.
    
    Students also reviewed scene organization using Parenting and Prefabs and we created a mini Obstacle system prefab together.
    
    Due to a poer outage, i wasn't able to ensure all students had a working copy of the code, but i have attached the script to their assignment for reference.

---
+++ Auto Runner - Level 1 - Trainer Edition/Assets/Scripts/Obstacle/ObstacleMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves the obstacle along a path determined by waypoints in the scene
+/// </summary>
+public class ObstacleMovement : MonoBehaviour
+{
+    #region Fields
+
+    [Header("Collection of waypoints to represent a path")]
+    public Transform[] waypoints;
+
+    [Header("Obstacle Movement Speed")]
+    public float speed;
+
+    //counter variable that determines the next waypoint to move to
+    int nextWaypoint = 1; 
+    #endregion
+
+    #region Monobehaviour Callbacks
+    private void Start()
+    {
+        //sets the obstacle position to the position of the first waypoint
+        InitObstacle();
+    }
+
+
+    private void Update()
+    {
+        //moves obstacle using Vector2.MoveTowards() from one waypoint to the next and loops
+        MoveByWaypoints();
+    }
+    #endregion
+
+    #region Private Methods
+    private void InitObstacle()
+    {
+        transform.position = waypoints[0].transform.position;
+    }
+    private void MoveByWaypoints()
+    {
+        //move to current waypoint
+        transform.position = Vector2.MoveTowards(transform.position,
+            waypoints[nextWaypoint].position, speed);
+
+        //when there move to the next
+        if (transform.position == waypoints[nextWaypoint].position)
+
+        {
+            Debug.Log("Move to next!");
+            nextWaypoint++;
+
+            //if at the end of the array, loop over
+            if (nextWaypoint >= waypoints.Length)
+            {
+                nextWaypoint = 0;
+            }
+        }
+    } 
+
+    #endregion
+}
